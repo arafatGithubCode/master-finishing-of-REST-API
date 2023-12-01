be864903e6c2a98ccba6615b5282e884ec48b2c6
arafatGithubCode <md489288@gmail.com>
2023-12-01 11:12:02 +0600

    Upload files, images, videos to database

--- app.js
+++ app.js
@@ -1,23 +1,69 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-const router = require("./routes/users.route");
+const mongoose = require("mongoose");
+const multer = require("multer");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use("/users", router);
-
-//home route
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+// Create register Schema and model
+const userSchema = new mongoose.Schema({
+  name: String,
+  images: [String], // Array to store multiple images
+  videos: [String], // Array to store multiple videos
 });
 
-//route not found error
+const User = mongoose.model("users", userSchema);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const fileName = Date.now() + "-" + file.originalname;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+//for one file field
+
+// app.post("/register", upload.array("images", 5), async (req, res) => {
+//   try {
+//     const newUser = new User({
+//       name: req.body.name,
+//       images: req.files.map(file => file.filename),
+//       videos: req.files.map(file => file.filename),
+//     });
+
+app.post(
+  "/register",
+  upload.fields([
+    { name: "images", maxCount: 5 },
+    { name: "videos", maxCount: 5 },
+  ]),
+  async (req, res) => {
+    try {
+      const newUser = new User({
+        name: req.body.name,
+        images: req.files["images"].map((file) => file.filename),
+        videos: req.files["videos"].map((file) => file.filename),
+      });
+      await newUser.save();
+      res.status(200).send(newUser);
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  }
+);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Route not found error
 app.use((req, res, next) => {
   res.status(404).json({
     statusCode: 404,
@@ -25,11 +71,11 @@ app.use((req, res, next) => {
   });
 });
 
-//server error
-app.use((err, erq, res, next) => {
+// Server error
+app.use((err, req, res, next) => {
   res.status(500).json({
     statusCode: 500,
-    message: "something broken",
+    message: "Something broke",
   });
 });
 
--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
 const PORT = 3000;
 
-app.listen(PORT, (req, res) => {
+//connectDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+    console.log("db is connected");
+  } catch (error) {
+    console.log("db is connected");
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+app.listen(PORT, async (req, res) => {
   console.log(`server is running at http://localhost:${PORT}`);
+  await connectDB();
 });
